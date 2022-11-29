f1fd976d4c01131e7079181f14d7d26174795184
YuriyCher <y0979441322@ukr.net>
2022-11-29 21:55:20 +0200

    бургер вже поряд

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,4 +1,33 @@
-(() => {
+// (() => {
+//     const mobileMenu = document.querySelector('.js-menu-container');
+//     const openMenuBtn = document.querySelector('.js-open-menu');
+//     const closeMenuBtn = document.querySelector('.js-close-menu');
+  
+//     const toggleMenu = () => {
+//       const isMenuOpen =
+//         openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//       mobileMenu.classList.toggle('is-open');
+  
+//       const scrollLockMethod = !isMenuOpen
+//         ? 'disableBodyScroll'
+//         : 'enableBodyScroll';
+//       bodyScrollLock[scrollLockMethod](document.body);
+//     };
+  
+//     openMenuBtn.addEventListener('click', toggleMenu);
+//     closeMenuBtn.addEventListener('click', toggleMenu);
+  
+//     // Close the mobile menu on wider screens if the device orientation changes
+//     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//       if (!e.matches) return;
+//       mobileMenu.classList.remove('is-open');
+//       openMenuBtn.setAttribute('aria-expanded', false);
+//       bodyScrollLock.enableBodyScroll(document.body);
+//     });
+//   })();
+  // <--mmjs-->
+  (() => {
     const mobileMenu = document.querySelector('.js-menu-container');
     const openMenuBtn = document.querySelector('.js-open-menu');
     const closeMenuBtn = document.querySelector('.js-close-menu');
@@ -26,4 +55,4 @@
       bodyScrollLock.enableBodyScroll(document.body);
     });
   })();
-  
+ 
